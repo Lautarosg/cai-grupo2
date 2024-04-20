cbbf1feae14ad571ca86e7a9940861d0de93611e
Lautaro s <lautarosalvatore98@gmail.com>
2024-04-20 18:32:46 -0300

    todavia falta alguna correccion pero en principio el crear usuario esta

--- TPCAI/Datos/Class1.cs
+++ TPCAI/Datos/Class1.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Datos
 {
-    public class Class1
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Text;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+    using System.Web.Helpers;
+    using TPCAI;
+
+
+    namespace YourNamespace
     {
+        class Program
+        {
+            public async Task<Usuario> CrearClienteAsync( UsuarioPostRequest usuarioPost)
+            {
+                // Create an instance of HttpClient
+
+                // Create an instance of HttpClient
+                using (HttpClient client = new HttpClient())
+                {
+                    // Define the URL of the API endpoint
+                    string apiUrl = "https://cai-tp.azurewebsites.net/api/Cliente/AgregarCliente";
+
+                    // Serialize your data object to JSON
+                    string jsonString = JsonSerializer.Serialize(usuarioPost);
+
+                    // Create an instance of StringContent with your JSON payload
+                    var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                    try
+                    {
+                        // Make the POST request
+                        HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+                        // Check if the request was successful
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string ResponseBody = await response.Content.ReadAsStringAsync();
+                            Usuario ResponseData = JsonSerializer.Deserialize<Usuario>(ResponseBody);
+                            Console.WriteLine("POST request was successful.");
+                            return ResponseData;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"POST request failed with status code {response.StatusCode}.");
+                            return null;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine($"An error occurred: {ex.Message}");
+                        return null;
+                    }
+
+                }
+                }
+            }
+        }
     }
-}
---
+++ TPCAI/Datos/UsuarioPostRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Datos.YourNamespace
+{
+    public class UsuarioPostRequest
+    {
+
+
+        public class UserData
+        {
+            public Guid IdUsuario { get; set; }
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public int Dni { get; set; }
+            public string Direccion { get; set; }
+            public string Telefono { get; set; }
+            public string Email { get; set; }
+            public DateTime FechaNacimiento { get; set; }
+            public string Host { get; set; }
+        }
+    }
+}
