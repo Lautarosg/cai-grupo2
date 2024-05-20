07cdc31c19ab518b145e7d8da5aac0cc3a329c9b
Lautaro s <lautarosalvatore98@gmail.com>
2024-05-20 16:16:31 -0300

    add cliente

---
+++ TPCAI/Datos/ClienteDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TPCAI
+{
+    public class ClienteDTO
+    {
+        public Guid IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Dni { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Host { get; set; }
+
+        public ClienteDTO(Guid idUsuario, string nombre, string apellido, int dni, string direccion, string telefono, string email, DateTime fechaNacimiento, string host)
+        {
+            IdUsuario = idUsuario;
+            Nombre = nombre;
+            Apellido = apellido;
+            Dni = dni;
+            Direccion = direccion;
+            Telefono = telefono;
+            Email = email;
+            FechaNacimiento = fechaNacimiento;
+            Host = host;
+        }
+    }
+}
---
+++ TPCAI/Datos/ClientePostRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    using System;
+
+    namespace Datos
+    {
+        public class ClientePostRequest
+        {
+            Guid _idUsuario;
+            string _nombre;
+            string _apellido;
+            int _dni;
+            string _direccion;
+            string _telefono;
+            string _email;
+            DateTime _fechaNacimiento;
+            string _host;
+
+            public  ClientePostRequest(Guid idUsuario, string nombre, string apellido, int dni, string direccion, string telefono, string email, DateTime fechaNacimiento, string host)
+            {
+                _idUsuario = idUsuario;
+                _nombre = nombre;
+                _apellido = apellido;
+                _dni = dni;
+                _direccion = direccion;
+                _telefono = telefono;
+                _email = email;
+                _fechaNacimiento = fechaNacimiento;
+                _host = host;
+            }
+
+            public Guid IdUsuario { get => _idUsuario; set => _idUsuario = value; }
+            public string Nombre { get => _nombre; set => _nombre = value; }
+            public string Apellido { get => _apellido; set => _apellido = value; }
+            public int Dni { get => _dni; set => _dni = value; }
+            public string Direccion { get => _direccion; set => _direccion = value; }
+            public string Telefono { get => _telefono; set => _telefono = value; }
+            public string Email { get => _email; set => _email = value; }
+            public DateTime FechaNacimiento { get => _fechaNacimiento; set => _fechaNacimiento = value; }
+            public string Host { get => _host; set => _host = value; }
+        }
+    }
+
+}
